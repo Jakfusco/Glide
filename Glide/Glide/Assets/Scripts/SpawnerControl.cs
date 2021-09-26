cb661a2a1c54d98f1eeecb3cc732843119aea0d9
Samuel McKinley <sammckin@uat.edu>
2021-09-25 17:26:08 -0700

    Spawn Script
    Added the Spawn script. Still needs to be adjusted

---
+++ Glide/Glide/Assets/Scripts/SpawnerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerControl : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public GameObject parentObject;
+    public int numberToSpawn;
+    public int limit = 20;
+    public float rate;
+
+    float spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimer = rate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (parentObject.transform.childCount < limit)
+        {
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0f)
+            {
+                for (int i = 0; i < numberToSpawn; i++)
+                {
+                    Instantiate(objectToSpawn, new Vector3(this.transform.position.x + GetModifier(), this.transform.position.y + GetModifier())
+                        , Quaternion.identity, parentObject.transform);
+                }
+                spawnTimer = rate;
+            }
+        }
+    }
+
+    float GetModifier()
+    {
+        float modifier = Random.Range(0f, 1f);
+        if (Random.Range(0, 2) > 0)
+            return -modifier;
+        else
+            return modifier;
+    }
+}
