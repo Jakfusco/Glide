58987024d6165ba225c628c2f8c34dacfb22feb7
TheGoatIsBetter <thegoatisbetter@gmail.com>
2021-09-26 13:20:04 -0700

    Added score and timer UI and functionality

---
+++ Glide/Glide/Assets/Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public float score = 0;
+    public float displayScore = 0;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayScore();
+    }
+
+    void DisplayScore()
+    {
+        score += Time.deltaTime * 10;
+        displayScore = Mathf.FloorToInt(score * 100);
+
+        scoreText.text = string.Format("Score: {0:00}", displayScore);
+    }
+}
---
+++ Glide/Glide/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float timer = 0;
+    public float minutes = 0;
+    public float seconds = 0;
+    public float milliseconds = 0;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayTimer();
+    }
+
+    void DisplayTimer()
+    {
+        timer += Time.deltaTime;
+        minutes = Mathf.FloorToInt(timer / 60);
+        seconds = Mathf.FloorToInt(timer % 60);
+        milliseconds = (timer % 1) * 1000;
+
+        timeText.text = string.Format("Time: {0:00}:{1:00}.{2:000}", minutes, seconds, milliseconds);
+    }
+}
