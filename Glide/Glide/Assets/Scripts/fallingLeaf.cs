5142e4b4436b60d4b3aeca34d05337a08f9784a5
UAT-JohnDV-III <90527388+UAT-JohnDV-III@users.noreply.github.com>
2021-09-25 17:17:35 -0700

    Commit (9/25/21)
    -Requires Oculus Integration, though translation shouldn't be too bad, I can assist on reading the code
    -Includes: Simple scene with a cube that acts like a hang glider without the speed changes, utilizes the direction of whatever hand that grabbed the "handle" on the X(?) axis, this can easily be changed. Some pickup objects, terrain.
    -Things to note: Do not scale or rotate the glider on the Z axis, this will ultimately cause visual glitches
    -Things that need to be done: Implement some sort of speed velocity for actual gliders.

---
+++ Glide/Glide/Assets/Scripts/fallingLeaf.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingLeaf : OVRGrabbable
+{
+    bool bFalling = true;
+    bool bAlreadyGrabbed = false;
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        bFalling = false;
+        gameObject.SetActive(false);
+        if (!bAlreadyGrabbed)
+        {
+            bAlreadyGrabbed = true;
+            //Do whatever events you need to do here
+        }
+    }
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(Vector3.zero, Vector3.zero);
+        bFalling = true;
+        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        this.enabled = false;
+    }
+
+    float flRotSpeed, flSinSpeed, flPosSpeed;
+
+    protected override void Start()
+    {
+        base.Start();
+        flSinSpeed = Random.Range(1.5f, 2.5f);
+        flRotSpeed = Random.Range(0.2f, 0.35f);
+        flPosSpeed = Random.Range(0.001f, 0.01f);
+    }
+
+    void Update()
+    {
+        if (bFalling)
+        {
+            float flSinValue = Mathf.Sin(Time.time * flSinSpeed);
+
+            transform.Rotate(-(flSinValue * flRotSpeed), 0.0f, flSinValue * flRotSpeed, Space.World);
+            transform.Translate(flSinValue * flPosSpeed, -0.001f, 0.0f, Space.Self);
+        }
+    }
+}
---
+++ Glide/Glide/Assets/Scripts/gliderHandle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gliderHandle : OVRGrabbable
+{
+    bool bGrabbed = false;
+    public GameObject parent;
+    GameObject player;
+    CharacterController controller;
+    OVRPlayerController VRcontroller;
+    OVRGrabber currentHand;
+    public Transform camOriginalPos;
+    float flDefaultGravity, flSpeed, flOriginalXRot;
+    protected override void Start()
+    {
+        base.Start();
+        player = GameObject.FindGameObjectWithTag("Player");
+        controller = player.GetComponent<CharacterController>();
+        flDefaultGravity = player.GetComponent<OVRPlayerController>().GravityModifier;
+        VRcontroller = player.GetComponent<OVRPlayerController>();
+    }
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+        currentHand = hand;
+        bGrabbed = true;
+        controller.enabled = false;
+        VRcontroller.enabled = false;
+        player.transform.SetParent(parent.transform);
+        transform.parent = parent.transform;
+        flOriginalXRot = currentHand.gameObject.transform.rotation.x;
+        player.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+        bGrabbed = false;
+        controller.enabled = true;
+        VRcontroller.enabled = true;
+        controller.velocity.Set(0.0f, 0.0f, 0.0f);
+        player.transform.SetParent(null);
+        transform.parent = parent.transform;
+        player.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    void Update()
+    {
+        if (bGrabbed)
+        {
+            float flNewRot = currentHand.gameObject.transform.rotation.x - flOriginalXRot;
+            float flSpeedMultiplier = 1.0f; //Mostly adjust this value for the gliding speed, although in fairness you could also touch the 0.06f
+            parent.transform.Translate(0.0f, 0.0f, 0.06f* flSpeedMultiplier, Space.Self);
+            parent.transform.rotation = new Quaternion(flNewRot, 0.0f, 0.0f, 1.5f);
+        }
+        if (transform.parent != parent.transform)
+        {
+            transform.parent = parent.transform;
+            transform.localPosition = new Vector3(1.08f, 0.0f, 0.0f);
+            transform.localScale = new Vector3(1.0f, 2.0f, 1.0f);
+            transform.localRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
